e456f4f56a06b1fc3048bd8d99d64feed3b07073
SeshHomeUbuntuPC <seshu.basava70@gmail.com>
2023-05-09 11:11:18 +0200

    Removed print from colorscheme and lualine

--- lua/seshu/plugins/lualine.lua
+++ lua/seshu/plugins/lualine.lua
@@ -99,7 +99,6 @@ local function get_native_lsp()
 	for _, client in ipairs(clients) do
 		local filetypes = client.config.filetypes
 		if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-			print(client.name)
 			return client.name
 		end
 	end
@@ -139,7 +138,6 @@ local function get_git_compare()
 end
 
 local color_theme = vim.g.colors_name
-print(color_theme)
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
