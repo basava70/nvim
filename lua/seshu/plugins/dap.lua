0b27a3cd2a1c8ff578ee05a72c4ffc67f5afb731
SeshHomeUbuntuPC <seshu.basava70@gmail.com>
2023-07-24 23:44:03 +0200

    Added breakpoint signs and changed nvim tree icons

---
+++ lua/seshu/plugins/dap.lua
@@ -0,0 +1,8 @@
+vim.fn.sign_define(
+	"DapBreakpoint",
+	{ text = "", texthl = "DapBreakpoint", linehl = "DapBreakpointLine", numhl = "DapBreakpoint" }
+)
+vim.fn.sign_define(
+	"DapStopped",
+	{ text = "", texthl = "DapStopped", linehl = "DapStoppedLine", numhl = "DapStopped" }
+)
