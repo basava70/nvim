2a5a8c60062dc8a6196e6b7e89c905412ab9fd0a
SeshuUbuntuHomePC <seshu.basava70@gmail.com>
2023-11-04 21:19:21 +0100

    Updated lualine with formatter, linter and lsp

--- lua/seshu/plugins/lualine.lua
+++ lua/seshu/plugins/lualine.lua
@@ -1,38 +1,92 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons", "linrongbin16/lsp-progress.nvim" },
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-		local function LspIcon()
-			local active_clients_count = #vim.lsp.get_active_clients()
-			return active_clients_count > 0 and " LSP" or ""
-		end
+		-------------------------------------------------------------
+		-- Lsp stuff --
+		local function getLspName()
+			local buf_clients = vim.lsp.buf_get_clients()
+			local buf_ft = vim.bo.filetype
+			if next(buf_clients) == nil then
+				return "  No servers"
+			end
+			local buf_client_names = {}
 
-		local function LspStatus()
-			return require("lsp-progress").progress({
-				format = function(messages)
-					local active_clients = vim.lsp.get_active_clients()
-					local client_count = #active_clients
-					if #messages > 0 then
-						return " LSP:" .. client_count .. " " .. table.concat(messages, " ")
-					end
-					if #active_clients <= 0 then
-						return " LSP:" .. client_count
-					else
-						local client_names = {}
-						for i, client in ipairs(active_clients) do
-							if client and client.name ~= "" then
-								table.insert(client_names, "[" .. client.name .. "]")
-								-- print("client[" .. i .. "]:" .. vim.inspect(client.name))
+			for _, client in pairs(buf_clients) do
+				if client.name ~= "null-ls" then
+					table.insert(buf_client_names, " " .. client.name)
+				end
+			end
+
+			local lint_s, lint = pcall(require, "lint")
+			if lint_s then
+				for ft_k, ft_v in pairs(lint.linters_by_ft) do
+					if type(ft_v) == "table" then
+						for _, linter in ipairs(ft_v) do
+							if buf_ft == ft_k then
+								table.insert(buf_client_names, " " .. linter)
 							end
 						end
-						return " LSP:" .. client_count .. " " .. table.concat(client_names, " ")
+					elseif type(ft_v) == "string" then
+						if buf_ft == ft_k then
+							table.insert(buf_client_names, ft_v)
+						end
 					end
-				end,
-			})
+				end
+			end
+
+			local ok, conform = pcall(require, "conform")
+			local formatters = table.concat(conform.formatters_by_ft[vim.bo.filetype], " ")
+			if ok then
+				for formatter in formatters:gmatch("%w+") do
+					if formatter then
+						table.insert(buf_client_names, "󰉠 " .. formatter)
+					end
+				end
+			end
+
+			local hash = {}
+			local unique_client_names = {}
+
+			for _, v in ipairs(buf_client_names) do
+				if not hash[v] then
+					unique_client_names[#unique_client_names + 1] = v
+					hash[v] = true
+				end
+			end
+			local language_servers = table.concat(unique_client_names, ", ")
+
+			return language_servers
 		end
+
+		local lsp = {
+			function()
+				return getLspName()
+			end,
+		}
+		-------------------------------------------------------------
+
+		local space = {
+			function()
+				return " "
+			end,
+		}
+
+		local filename = {
+			"filename",
+		}
+
+		local filetype = {
+			"filetype",
+			icons_enabled = false,
+		}
+
+		local branch = {
+			"branch",
+		}
 
 		lualine.setup({
 			sections = {
@@ -42,20 +96,28 @@ return {
 						icon = { "" },
 					},
 				},
-				lualine_c = {
-					-- invoke `progress` here.
-					LspStatus,
-				},
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "diagnostics" },
 				lualine_x = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
 						color = { fg = "#ff9e64" },
 					},
-					{ "encoding" },
-					{ "fileformat" },
 					{ "filetype" },
 				},
+				lualine_y = { "filename" },
+				lualine_z = {
+					lsp,
+				},
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
 			},
 		})
 		-- listen lsp-progress event and refresh lualine
