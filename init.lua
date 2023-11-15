aa0db572e395ece0f927377b8ca23af78088e319
Seshadri Basava <sebasa001@binf02m086.local>
2023-11-15 02:18:53 +0100

    setting up lazyvim

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-require("seshu.core")
-require("seshu.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/seshu/core/init.lua
+++
@@ -1,2 +0,0 @@
-require("seshu.core.keymaps")
-require("seshu.core.options")
